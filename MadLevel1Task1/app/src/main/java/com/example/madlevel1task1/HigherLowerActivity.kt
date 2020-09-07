216d3aec8214cf65e5f090584feae7f25f12ddaa
Maarten Neesen <c4theslime@gmail.com>
2020-09-07 10:49:28 +0200

    Added viewbinding and initialized variables for dice game

--- MadLevel1Task1/app/src/main/java/com/example/madlevel1task1/HigherLowerActivity.kt
+++ MadLevel1Task1/app/src/main/java/com/example/madlevel1task1/HigherLowerActivity.kt
@@ -6,9 +6,11 @@ import com.example.madlevel1task1.databinding.ActivityHigherLowerBinding
 
 class HigherLowerActivity : AppCompatActivity() {
     private lateinit var binding: ActivityHigherLowerBinding
+    private var currentThrow = 1
+    private var lastThrow = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        setContentView(binding.root)
     }
 }
