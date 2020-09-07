15a2b1b2e8ee2c43073d96f7c307b1b83eb3cad7
Maarten Neesen <c4theslime@gmail.com>
2020-09-07 11:05:15 +0200

    Added initial state for dice game, also adjusted position of text in app

--- MadLevel1Task1/app/src/main/java/com/example/madlevel1task1/HigherLowerActivity.kt
+++ MadLevel1Task1/app/src/main/java/com/example/madlevel1task1/HigherLowerActivity.kt
@@ -11,6 +11,25 @@ class HigherLowerActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityHigherLowerBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        initViews()
+    }
+
+    private fun initViews() {
+        updateUI()
+    }
+
+    private fun updateUI() {
+        when (currentThrow) {
+            1 -> binding.dice.setImageResource(R.drawable.dice1)
+            2 -> binding.dice.setImageResource(R.drawable.dice2)
+            3 -> binding.dice.setImageResource(R.drawable.dice3)
+            4 -> binding.dice.setImageResource(R.drawable.dice4)
+            5 -> binding.dice.setImageResource(R.drawable.dice6)
+            6 -> binding.dice.setImageResource(R.drawable.dice6)
+        }
+        binding.lastThrow.text = getString(R.string.lastThrow_text, lastThrow)
+
     }
 }
